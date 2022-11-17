e04352729f70917082e1fc7fe6f704e25aa37349
Matthieu Monsch <matthieu.monsch@gmail.com>
2022-11-17 09:49:40 -0800

    Add brotli support

--- opvious/executors.py
+++ opvious/executors.py
@@ -17,6 +17,7 @@ with the License.  You may obtain a copy of the License at
   under the License.
 """
 
+import json
 from typing import Any, Mapping, Optional, Protocol
 
 
@@ -33,7 +34,10 @@ _TRACE_HEADER = "opvious-trace"
 
 class ApiError(Exception):
     def __init__(self, status, trace, errors=None, extensions=None):
-        super().__init__(f"API call failed with status {status} ({trace})")
+        msg = f"API call failed with status {status} ({trace})"
+        if errors:
+            msg += ": " + ", ".join(e["message"] for e in errors)
+        super().__init__(msg)
         self.status = status
         self.trace = trace
         self.errors = errors
@@ -49,12 +53,22 @@ def _extract_response_data(
     return body["data"]
 
 
+_BROTLI_QUALITY = 4
+
+
+_COMPRESSION_THRESHOLD = 2**16
+# 64 kiB
+
+
 def aiohttp_executor(url: str, auth: str) -> Executor:
     import aiohttp
+    import brotli
 
-    headers = {
+    default_headers = {
+        "accept-encoding": "br;q=1.0, gzip;q=0.5, *;q=0.1",
         "authorization": auth,
-        "opvious-client": "Python SDK (aiohttp)",
+        "content-type": "application/json",
+        "opvious-sdk": "Python (aiohttp)",
     }
 
     class AiohttpExecutor:
@@ -63,10 +77,20 @@ def aiohttp_executor(url: str, auth: str) -> Executor:
         async def execute(
             self, query: str, variables: Mapping[str, Any]
         ) -> Any:
-            data = {"query": query, "variables": variables}
+            data = json.dumps({"query": query, "variables": variables})
+            if len(data) > _COMPRESSION_THRESHOLD:
+                headers = {"content-encoding": "br"}
+                headers.update(default_headers)
+                data = brotli.compress(
+                    data.encode("utf8"),
+                    mode=brotli.MODE_TEXT,
+                    quality=_BROTLI_QUALITY,
+                )
+            else:
+                headers = default_headers
             async with aiohttp.ClientSession(headers=headers) as session:
                 endpoint = url + _GRAPHQL_ENDPOINT
-                async with session.post(endpoint, json=data) as res:
+                async with session.post(endpoint, data=data) as res:
                     trace = res.headers.get(_TRACE_HEADER)
                     body = await res.json()
                     return _extract_response_data(res.status, trace, body)
