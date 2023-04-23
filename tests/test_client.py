13cfb733726965820ec3a811debdc724d31108ef
Matthieu Monsch <matthieu.monsch@gmail.com>
2023-04-23 08:34:32 -0700

    Simplify client test setup

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,10 +3,7 @@ import os
 import pytest
 
 
-client = opvious.Client.from_token(
-    token=os.environ.get("OPVIOUS_AUTHORIZATION", ""),
-    domain=os.environ.get(opvious.ClientSettings.DOMAIN.value),
-)
+client = opvious.Client.from_environment()
 
 
 @pytest.mark.skipif(
