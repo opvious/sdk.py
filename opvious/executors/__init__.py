7791e5e6169fc13f651e491153f6c60e1e0f1a38
Matthieu Monsch <matthieu.monsch@gmail.com>
2023-07-26 09:43:50 -0700

    Update

--- opvious/executors/__init__.py
+++ opvious/executors/__init__.py
@@ -1,4 +1,6 @@
 import base64
+import io
+import pandas as pd
 import sys
 from typing import Optional
 
@@ -26,6 +28,7 @@ __all__ = [
     "JsonExecutorResult",
     "JsonSeqExecutorResult",
     "PlainTextExecutorResult",
+    "fetch_csv",
     "fetch_text",
 ]
 
@@ -84,25 +87,30 @@ def authorization_header(token: str) -> str:
     return f"Bearer {token}"
 
 
-async def fetch_text(
-    url: str,
-    headers: Optional[Headers] = None,
-) -> str:
+async def fetch_text(url: str) -> str:
     """Fetches a URL's contents as plain text
-
-    This convenience method is provided as a portable way to fetch data from
-    notebooks. Using the default `pandas.read_csv` for example does not support
-    `https://` URLs from JupyterLite (Pyodide).
 
     Args:
         url: The URL to fetch (via GET)
-        headers: Optional headers to include in the request
     """
     # We specify an invalid endpoint URL so that default headers are never
     # included in the request. Otherwise it causes certain requests to fail
     # (e.g. when fetching gists).
     executor = default_executor("ignored://")
-    async with executor.execute(
-        PlainTextExecutorResult, url, headers=headers
-    ) as res:
+    async with executor.execute(PlainTextExecutorResult, url) as res:
         return await res.text()
+
+
+async def fetch_csv(url: str, **kwargs) -> str:
+    """Fetches a CSV from a URL
+
+    This convenience method is provided as a portable way to fetch data from
+    notebooks. Using the default `pandas.read_csv` does not support `https://`
+    URLs from JupyterLite (Pyodide).
+
+    Args:
+        url: The URL to fetch (via GET)
+        **kwargs: Keyword arguments forwarded to `pandas.read_csv`
+    """
+    text = await fetch_text(url)
+    return pd.read_csv(io.StringIO(text), **kwargs)
--- tests/test_executors.py
+++ tests/test_executors.py
@@ -51,4 +51,11 @@ class TestExecutors:
         text = await opvious.executors.fetch_text(
             "https://gist.githubusercontent.com/mtth/82a21baacba4827bc1710b7526775315/raw/4e2b17c1509dfd40595f47167e8d859fa4ec8334/sudoku.md"  # noqa
         )
-        assert "defining" in text
+        assert "defining" in text.read()
+
+    @pytest.mark.asyncio
+    async def test_fetch_csv(self):
+        df = await opvious.executors.fetch_csv(
+            "https://gist.githubusercontent.com/mtth/f59bdde8694223c06f77089b71b48d17/raw/6f1568cb2ff69450f06e3b8045d504af74bb701f/fpl-2023-07-26.csv"  # noqa
+        )
+        assert len(df) == 605
