b654c440fac51e02644bc878adc0c6907a066f3a
Matthieu Monsch <matthieu.monsch@gmail.com>
2024-08-06 20:06:26 -0700

    fixup! refactor: reorganize source files

--- scripts/specifications.sh
+++ scripts/specifications.sh
@@ -6,40 +6,40 @@ set -o pipefail
 shopt -s nullglob
 
 usage() {
-  local cmd="${0##*/}"
-  cat <<EOF
-Manage specifications test resources
-
-Usage:
-  $cmd register # Register all test specifications
-EOF
+	local cmd="${0##*/}"
+	cat <<-EOF
+		Manage specifications test resources
+		
+		Usage:
+		  $cmd register # Register all test specifications
+	EOF
 }
 
 fail() { # MSG
-  echo "$1" >&2 && exit 1
+	echo "$1" >&2 && exit 1
 }
 
 register_specifications() {
-  for p in tests/notebooks/*.ipynb; do
-    poetry run python -m opvious register-notebook "$p"
-  done
-  for p in tests/sources/*.md; do
-    poetry run python -m opvious register-sources "$p"
-  done
+	for p in tests/opvious/notebooks/*.ipynb; do
+		poetry run python -m opvious register-notebook "$p"
+	done
+	for p in tests/opvious/sources/*.md; do
+		poetry run python -m opvious register-sources "$p"
+	done
 }
 
 main() {
-  if (($# == 0)); then
-    usage
-    exit 3
-  fi
-  subcmd="$1"
-  shift
+	if (($# == 0)); then
+		usage
+		exit 3
+	fi
+	subcmd="$1"
+	shift
 
-  case $subcmd in
-    register) register_specifications ;;
-    *) fail 'invalid subcommand' ;;
-  esac
+	case $subcmd in
+		register) register_specifications ;;
+		*) fail 'invalid subcommand' ;;
+	esac
 }
 
 main "$@"
--- scripts/version.sh
+++ scripts/version.sh
@@ -6,61 +6,61 @@ set -o pipefail
 shopt -s nullglob
 
 usage() {
-  local cmd="${0##*/}"
-  cat <<EOF
-Manage package version
+	local cmd="${0##*/}"
+	cat <<-EOF
+		Manage package version
 
-All versions declared in \`pyproject.toml\` should be release candidates. This
-script can be used to extract the final version during releases.
+		All versions declared in \`pyproject.toml\` should be release candidates.
+		This script can be used to extract the final version during releases.
 
-Usage:
-  $cmd check # Check version
-  $cmd show [-t] # Show version, optionally trimmed of suffix
-EOF
+		Usage:
+		  $cmd check # Check version
+		  $cmd show [-t] # Show version, optionally trimmed of suffix
+	EOF
 }
 
 fail() { # MSG
-  echo "$1" >&2 && exit 1
+	echo "$1" >&2 && exit 1
 }
 
 show_version() {
-  local trim=0
-  while getopts t o "$@"; do
-    case "$o" in
-      t) trim=1 ;;
-      *) fail 'bad option'
-    esac
-  done
+	local trim=0
+	while getopts t o "$@"; do
+		case "$o" in
+			t) trim=1 ;;
+			*) fail 'bad option'
+		esac
+	done
 
-  local version="$(poetry version -s)"
-  if ! [[ $version =~ (.+)rc.+ ]]; then
-    fail "unexpected version: $version"
-  fi
+	local version="$(poetry version -s)"
+	if ! [[ $version =~ (.+)rc.+ ]]; then
+		fail "unexpected version: $version"
+	fi
 
-  if (( $trim == 0 )); then
-    echo "$version"
-  else
-    echo "${BASH_REMATCH[1]}"
-  fi
+	if (( $trim == 0 )); then
+		echo "$version"
+	else
+		echo "${BASH_REMATCH[1]}"
+	fi
 }
 
 check_version() {
-  show_version >/dev/null
+	show_version >/dev/null
 }
 
 main() {
-  if (($# == 0)); then
-    usage
-    exit 3
-  fi
-  subcmd="$1"
-  shift
+	if (($# == 0)); then
+		usage
+		exit 3
+	fi
+	subcmd="$1"
+	shift
 
-  case $subcmd in
-    check) check_version ;;
-    show) show_version "$@" ;;
-    *) fail 'invalid subcommand' ;;
-  esac
+	case $subcmd in
+		check) check_version ;;
+		show) show_version "$@" ;;
+		*) fail 'invalid subcommand' ;;
+	esac
 }
 
 main "$@"
