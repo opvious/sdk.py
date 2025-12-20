8663e64130d7bad14928e286ad5660c40cf2a654
Matthieu Monsch <1216372+mtth@users.noreply.github.com>
2025-12-20 11:43:04 -0800

    fix: keep output dataframe columns (#147)
    It is inconvenient to handle the case of missing columns when a variable
    or constraint only has zero values.

---
+++ tests/data_test.py
@@ -0,0 +1,29 @@
+from opvious.data.outlines import ProblemOutline, TensorOutline
+from opvious.data.solves import SolveOutputs
+
+
+class TestSolveOutputs:
+    def test_variable_empty(self):
+        outputs = SolveOutputs(
+            problem_outline=ProblemOutline(
+                objectives={},
+                dimensions={},
+                parameters={},
+                variables={
+                    "foo": TensorOutline(
+                        label="foo",
+                        lower_bound=None,
+                        upper_bound=None,
+                        is_integral=False,
+                        bindings=[],
+                        derivation_kind=None,
+                    ),
+                },
+                constraints={},
+            ),
+            raw_variables=[{"label": "foo", "entries": []}],
+            raw_constraints=[],
+        )
+        df = outputs.variable("foo")
+        assert df.columns.tolist() == ["value"]
+        assert len(df["value"]) == 0
