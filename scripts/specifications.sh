2fe231587c9acbf0b15329cfb54bb599a77b6a82
Matthieu Monsch <matthieu.monsch@gmail.com>
2025-12-19 21:29:13 -0800

    fixup! d060a93

--- scripts/specifications.sh
+++ scripts/specifications.sh
@@ -20,10 +20,10 @@ fail() { # MSG
 }
 
 register_specifications() {
-	for p in tests/opvious/notebooks/*.ipynb; do
+	for p in tests/notebooks/*.ipynb; do
 		poetry run python -m opvious register-notebook "$p"
 	done
-	for p in tests/opvious/sources/*.md; do
+	for p in tests/sources/*.md; do
 		poetry run python -m opvious register-sources "$p"
 	done
 }
